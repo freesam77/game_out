d09861672d506f16424ea46b3cab4be29d21b0ac
Samuel Razali <samuelrazali@gmail.com>
2018-05-09 03:12:57 +1000

    corrected edit and signup username for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,11 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
-    # before_action :configure_permitted_parameters, if: :devise_controller?
-
-    # protected
-
-    # def configure_permitted_parameters
-    #     devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:username)}
-    #     devise_parameter_sanitizer.for(:sign_in) {|u| u.permit(:email, :password, :remember_me)}
-    #     devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:username, :password, :email)}
-    # end
+    protected
+  
+    def configure_permitted_parameters
+      added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -6,7 +6,7 @@ class Game < ApplicationRecord
     has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-    has_attached_file :file
+    # has_attached_file :file
 
     def self.search(search)
         if search
