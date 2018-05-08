a1a3ba1fa3abd6ab764bc795a6ab21c601d7a522
Samuel Razali <samuelrazali@gmail.com>
2018-05-08 11:35:35 +1000

    added attr_accessor :image_file_name to fix heroku error

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,5 @@
 class Game < ApplicationRecord
+    attr_accessor :image_file_name
     belongs_to :user
 
     has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
