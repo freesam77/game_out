644b53eb554a6963015631b84167ee44886a50da
Samuel Razali <samuelrazali@gmail.com>
2018-05-10 08:15:03 +1000

    modified

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -38,7 +38,7 @@ class GamesController < ApplicationController
 
     respond_to do |format|
       if @game.save
-        format.html { redirect_to games_url, notice: 'Game was successfully created.' }
+        format.html { redirect_to @game, notice: 'Game was successfully created.' }
         format.json { render :show, status: :created, location: @game }
       else
         format.html { render :new }
